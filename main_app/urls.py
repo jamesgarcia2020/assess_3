22c20afd0491ece43b1b4b555ced9ea85e2b743c
James Garcia <jamesgarcia@Jamess-MacBook-Air.local>
2020-09-25 12:54:49 -0700

    completed 3.5

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addwidget/', views.WidgetCreate.as_view(), name='add_widget'),
+    path('<int:pk>/delete/', views.WidgetDelete.as_view(), name='widgets_delete'),
+    path('<int:pk>/update/', views.WidgetUpdate.as_view(), name='widgets_update'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Widget
 # Create your views here.
 
@@ -6,3 +7,20 @@ from .models import Widget
 def index(request):
     widgets = Widget.objects.all()
     return render(request, 'index.html', {'widgets': widgets})
+
+
+class WidgetCreate(CreateView):
+  model = Widget
+  fields = "__all__"
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = '/'
+
+class WidgetUpdate(UpdateView):
+    model = Widget
+    fields = "__all__"
